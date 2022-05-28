9d8da3f64c5b91d3d423de75c469ab761ac1e8ec
Osiris Germosen <osirisgermosen@outlook.com>
2022-05-28 15:16:23 -0400

    add new function

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+func sum(a, b int) {
+	return a + b
+}
+
 func main() {
 	number := 25
 	age := 32
 	fmt.Println("The number is: ", number)
 	fmt.Println("My age is: ", age)
+	fmt.Println("The sum 5 + 6 is:", sum(5, 6))
 }
